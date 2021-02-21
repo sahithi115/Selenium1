f7903b0e30ddbefcc07a9054a89fec82a0670431
sahithi <sumsah62@gmail.com>
2021-02-20 18:12:06 -0700

    automation test scripts for tradev

--- Assignment.java
+++ Assignment.java
@@ -1,5 +1,7 @@
 package package1;
 
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -31,23 +33,35 @@ public class Assignment {
 //		driver.findElement(By.linkText("Canadian Opportunities")).click();
 //	}
 //	
-//	@Test
-//	public void scenario3() {
-//		driver.get("https://jobs.lever.co/traderev");
+	@Test
+	public void scenario3() {
+		driver.get("https://jobs.lever.co/traderev");
 //		Select s = new Select(driver.findElement(By.xpath("//div[contains(text(),'Location')]")));
 //		s.selectByVisibleText("Toronto, Ontario, Canada");
-//	}
+	    driver.findElement(By.xpath("//div[contains(text(),'Location')]")).click();
+	    WebElement toronto = driver.findElement(By.xpath("//a[contains(text(),'Toronto, Ontario, Canada')]"));
+		WebDriverWait wait = new WebDriverWait(driver,30);
+		wait.until(ExpectedConditions.elementToBeClickable(toronto));
+		toronto.click();
+  }
 	
 	@Test 
 	public void scenario4() throws Throwable {
 		driver.get("https://jobs.lever.co/traderev");
+		driver.findElement(By.xpath("//div[contains(text(),'Location')]")).click();
+	    WebElement toronto = driver.findElement(By.xpath("//a[contains(text(),'Toronto, Ontario, Canada')]"));
+		WebDriverWait wait = new WebDriverWait(driver,30);
+		wait.until(ExpectedConditions.elementToBeClickable(toronto));
+		toronto.click();
 		driver.findElement(By.xpath("//div[contains(text(),'Team')]")).click();
 		//Select sai = new Select(driver.findElement(By.xpath("//div[contains(text(),'Team')]")));
 		//sai.selectByVisibleText("Engineering");
 		WebElement engineering = driver.findElement(By.xpath("//a[contains(text(),'Engineering')]"));
-		WebDriverWait wait = new WebDriverWait(driver,30);
-		wait.until(ExpectedConditions.elementToBeClickable(engineering));
+		WebDriverWait Wait = new WebDriverWait(driver,30);
+		Wait.until(ExpectedConditions.elementToBeClickable(engineering));
 		engineering.click();
+		List<WebElement> optionCount = driver.findElements(By.xpath("//span[contains(text(),'Toronto, Ontario, Canada')]"));
+		System.out.println(optionCount.size());
 		Thread.sleep(1000);
 	}
 	
